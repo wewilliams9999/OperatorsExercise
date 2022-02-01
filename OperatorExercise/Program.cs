f03bbf267ef1790061b500d357ea4b6f5eca65e0
wewilliams9999 <williamswallise@gmail.com>
2022-01-31 20:25:48 -0600

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+        }
+    }
+}
